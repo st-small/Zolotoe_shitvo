df06bfdc843b10fbf02d32dfb4de0130b5e1e356
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-02-24 19:18:06 +0200

    Реализовал FSPagerView

--- Zolotoe_shitvo/Zolotoe_shitvo/SiSCategoriesViewController.m
+++ Zolotoe_shitvo/Zolotoe_shitvo/SiSCategoriesViewController.m
@@ -12,15 +12,16 @@
 #import "SiSPersistentManager.h"
 #import "SiSOneCategoryViewController.h"
 
+#import <FSPagerView/FSPagerView-Swift.h>
+
 #define targetHeight 170
 
-@interface SiSCategoriesViewController ()
+@interface SiSCategoriesViewController () <FSPagerViewDataSource, FSPagerViewDelegate>
 
-typedef void(^myCompletion)(BOOL);
 
 @property (strong, nonatomic) NSMutableArray* imagesArray;
 @property (strong, nonatomic) NSMutableArray* imagesIDs;
-@property (weak, nonatomic) IBOutlet UICollectionView *itemsTable;
+//@property (weak, nonatomic) IBOutlet UICollectionView *itemsTable;
 
 @property (nonatomic, assign) CGPoint scrollingPoint, endPoint;
 @property (nonatomic, strong) NSTimer *scrollingTimer;
@@ -35,6 +36,10 @@ typedef void(^myCompletion)(BOOL);
 @property (strong, nonatomic) UIView* backgroundView;
 @property (strong, nonatomic) UIView* alertView;
 
+#pragma mark - FSPagerView -
+@property (weak, nonatomic) IBOutlet FSPagerView* pagerView;
+
+
 @end
 
 @implementation SiSCategoriesViewController
@@ -44,18 +49,18 @@ typedef void(^myCompletion)(BOOL);
     
     self.mitres = [NSMutableArray array];
     
-    UITapGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self
-                                                                                action:@selector(tappedCell:)];
-    doubleTap.numberOfTapsRequired = 1;
-    doubleTap.delaysTouchesBegan = YES;
-    [self.itemsTable addGestureRecognizer:doubleTap];
+//    UITapGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self
+//                                                                                action:@selector(tappedCell:)];
+//    doubleTap.numberOfTapsRequired = 1;
+//    doubleTap.delaysTouchesBegan = YES;
+//    [self.itemsTable addGestureRecognizer:doubleTap];
     
     self.imagesArray = [NSMutableArray array];
     self.imagesIDs = [NSMutableArray array];
     
     for (SiSProduct* obj in self.offerProducts) {
 
-        [self.imagesArray addObject:[self resizingImage:obj.img]];
+        [self.imagesArray addObject:obj.img];
         [self.imagesIDs addObject:obj.idProduct];
         
     }
@@ -68,19 +73,20 @@ typedef void(^myCompletion)(BOOL);
             [self.imagesArray addObjectsFromArray:temp];
             [self.imagesIDs addObjectsFromArray:temp2];
             //NSLog(@"%d", self.imagesArray.count);
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self.itemsTable reloadData];
-            });
+            
         }
     });
+    
+    [self.pagerView registerClass:[FSPagerViewCell class] forCellWithReuseIdentifier:@"cell"];
+    self.pagerView.itemSize = CGSizeMake(190, 190);
+    self.pagerView.interitemSpacing = 16;
+    self.pagerView.automaticSlidingInterval = 3.f;
     
 //    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
 //        
 //        self.mitres = [[SiSPersistentManager sharedManager] getCategoriesProductsOfCategory:6 andName:@"Mitres"];
 //
 //    });
-    
-    [self scrollSlowly];
 
 }
 
@@ -99,91 +105,42 @@ typedef void(^myCompletion)(BOOL);
  
 }
 
-#pragma mark UICollectionViewDataSource
+#pragma mark - FSPagerViewDataSource
 
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    
-    return 1;
-}
-
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+- (NSInteger)numberOfItemsInpagerView:(FSPagerView *)pagerView {
     
     return self.imagesArray.count;
 }
 
-- (UICollectionViewCell*)collectionView:(UICollectionView*)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+- (FSPagerViewCell *)pagerView:(FSPagerView *)pagerView
+            cellForItemAtIndex:(NSInteger)index {
     
-    SiSCollectionViewCell* cell = (SiSCollectionViewCell*)[collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
-    cell.imgView.image = self.imagesArray[indexPath.row];
-    cell.idProduct = self.imagesIDs[indexPath.row];
+    FSPagerViewCell *cell = [pagerView dequeueReusableCellWithReuseIdentifier:@"cell" atIndex:index];
+    
+    UIImage* img = self.imagesArray[index];
+    cell.imageView.image = img;
+    cell.imageView.contentMode = UIViewContentModeScaleAspectFill;
+    cell.imageView.clipsToBounds = YES;
+    //cell.textLabel.text = self.imagesIDs[index];
     
     return cell;
 }
 
-#pragma mark UICollectionViewDelegate
+#pragma mark - FSPagerView Delegate
 
-- (CGSize)collectionView:(UICollectionView*)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-    
-    UIImage* img = self.imagesArray[indexPath.row];
-    CGSize origin = CGSizeMake(img.size.width, img.size.height);
-    
-    return origin;
+- (void)pagerView:(FSPagerView *)pagerView didSelectItemAtIndex:(NSInteger)index
+{
+    [pagerView deselectItemAtIndex:index animated:YES];
+    [pagerView scrollToItemAtIndex:index animated:YES];
+    //self.pageControl.currentPage = index;
 }
 
-#pragma mark Private methods
-
-- (UIImage*)resizingImage:(UIImage*)image {
-    
-    CGFloat scaleFactor = targetHeight / image.size.height;
-    CGFloat targetWidth = image.size.width * scaleFactor;
-    CGSize targetSize = CGSizeMake(targetWidth, targetHeight);
-    UIImage* scaledImage = [SiSCategoriesViewController imageWithImage:image scaledToSize:targetSize];
-    
-    return scaledImage;
+- (void)pagerViewDidScroll:(FSPagerView *)pagerView {
+//    if (self.pageControl.currentPage != pagerView.currentIndex) {
+//        self.pageControl.currentPage = pagerView.currentIndex;
+//    }
 }
 
-+ (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
-    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
-    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
-    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return newImage;
-}
-
-#pragma mark Animation methods
-// Анимации CollectionView
-- (void)scrollSlowly {
-    self.endPoint = CGPointMake(0, self.itemsTable.frame.size.width);
-    self.scrollingPoint = CGPointMake(0, 0);
-    self.scrollingTimer = [NSTimer scheduledTimerWithTimeInterval:0.015 target:self selector:@selector(scrollSlowlyToPoint) userInfo:nil repeats:YES];
-}
-
-- (void)scrollSlowlyToPoint {
-    
-    self.itemsTable.contentOffset = self.scrollingPoint;
-    if (CGPointEqualToPoint(self.scrollingPoint, self.endPoint)) {
-        [self.scrollingTimer invalidate];
-    }
-    self.scrollingPoint = CGPointMake(self.scrollingPoint.x + 0.3, 0);
-}
-
-// Обработка нажатия выбора картинки в CollectionView
-- (IBAction)tappedCell:(id)sender {
-    
-    CGPoint tappedPoint = [sender locationInView:self.itemsTable];
-    NSIndexPath *tappedCellPath = [self.itemsTable indexPathForItemAtPoint:tappedPoint];
-    
-    if (tappedCellPath) {
-        
-        SiSCollectionViewCell* cell = (SiSCollectionViewCell*)[self.itemsTable cellForItemAtIndexPath:tappedCellPath];
-        [self.itemsTable selectItemAtIndexPath:tappedCellPath
-                                          animated:YES
-                                    scrollPosition:UICollectionViewScrollPositionNone];
-        
-        NSLog(@"the cell tag is %@", cell.idProduct);
-        
-    }
-}
 
 
 // Обработка нажатия кнопки выбора раздела
@@ -381,6 +338,94 @@ typedef void(^myCompletion)(BOOL);
                                                                     
                                                                 }];
 }
+
+#pragma mark - UICollectonView Methods an Animations
+//#pragma mark UICollectionViewDataSource
+//
+//- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+//    
+//    return 1;
+//}
+//
+//- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+//    
+//    return self.imagesArray.count;
+//}
+//
+//- (UICollectionViewCell*)collectionView:(UICollectionView*)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+//    
+//    SiSCollectionViewCell* cell = (SiSCollectionViewCell*)[collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+//    cell.imgView.image = self.imagesArray[indexPath.row];
+//    cell.idProduct = self.imagesIDs[indexPath.row];
+//    
+//    return cell;
+//}
+//
+//#pragma mark UICollectionViewDelegate
+//
+//- (CGSize)collectionView:(UICollectionView*)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+//    
+//    UIImage* img = self.imagesArray[indexPath.row];
+//    CGSize origin = CGSizeMake(img.size.width, img.size.height);
+//    
+//    return origin;
+//}
+//
+//#pragma mark Private methods
+//
+//- (UIImage*)resizingImage:(UIImage*)image {
+//    
+//    CGFloat scaleFactor = targetHeight / image.size.height;
+//    CGFloat targetWidth = image.size.width * scaleFactor;
+//    CGSize targetSize = CGSizeMake(targetWidth, targetHeight);
+//    UIImage* scaledImage = [SiSCategoriesViewController imageWithImage:image scaledToSize:targetSize];
+//    
+//    return scaledImage;
+//}
+//
+//+ (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
+//    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+//    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+//    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+//    return newImage;
+//}
+//
+//#pragma mark Animation methods
+//// Анимации CollectionView
+//- (void)scrollSlowly {
+//    self.endPoint = CGPointMake(0, self.itemsTable.frame.size.width);
+//    self.scrollingPoint = CGPointMake(0, 0);
+//    self.scrollingTimer = [NSTimer scheduledTimerWithTimeInterval:0.015 target:self selector:@selector(scrollSlowlyToPoint) userInfo:nil repeats:YES];
+//}
+//
+//- (void)scrollSlowlyToPoint {
+//    
+//    self.itemsTable.contentOffset = self.scrollingPoint;
+//    if (CGPointEqualToPoint(self.scrollingPoint, self.endPoint)) {
+//        [self.scrollingTimer invalidate];
+//    }
+//    self.scrollingPoint = CGPointMake(self.scrollingPoint.x + 0.3, 0);
+//}
+//
+//// Обработка нажатия выбора картинки в CollectionView
+//- (IBAction)tappedCell:(id)sender {
+//    
+//    CGPoint tappedPoint = [sender locationInView:self.itemsTable];
+//    NSIndexPath *tappedCellPath = [self.itemsTable indexPathForItemAtPoint:tappedPoint];
+//    
+//    if (tappedCellPath) {
+//        
+//        SiSCollectionViewCell* cell = (SiSCollectionViewCell*)[self.itemsTable cellForItemAtIndexPath:tappedCellPath];
+//        [self.itemsTable selectItemAtIndexPath:tappedCellPath
+//                                      animated:YES
+//                                scrollPosition:UICollectionViewScrollPositionNone];
+//        
+//        NSLog(@"the cell tag is %@", cell.idProduct);
+//        
+//    }
+//}
+
 
 
 
